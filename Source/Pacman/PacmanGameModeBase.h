6f1b8d8a15ffd19ab6f7252b412389402d537775
Sentyabov Sergey <sergsenta1995@gmail.com>
2018-09-08 21:03:00 +0300

    Initial commit

---
+++ Source/Pacman/PacmanGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PacmanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PACMAN_API APacmanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
